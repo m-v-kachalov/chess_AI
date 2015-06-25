7bcbb32278af9a11d75c65ffec1b2139282367b4
m-v-kachalov <m.v.kachalov@gmail.com>
2015-06-25 09:12:31 -0700

    good ideas for board

--- board.py
+++ board.py
@@ -1,14 +1,21 @@
 import numpy as np
 
+class pieceTypes:
+	PAWN = 1
+	KNIGHT = 2
+	BISHOP = 3
+	ROOK = 4
+	QUEEN = 5
+	KING = 6
 
 class chessPiece(object):
-	def __init__(self, name):
-		self.name = name
+	def __init__(self, pieceType):
+		self.pieceType = pieceType
 
 
-# board = {'a': {}, '' : {} }
+
+# what is the best way to represent a board??
 Z = np.zeros((8,8),dtype=chessPiece)
-# Z[1::2,::2] = '1'
-Z[0,0] = chessPiece('hello')
+Z[0,0] = chessPiece(pieceTypes.PAWN)
 # Z[::2,1::2] = '1'
-print Z[0,0].name
+print Z[0,0].pieceType
